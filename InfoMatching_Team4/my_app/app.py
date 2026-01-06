9ef8a08c4be28085a24e97a33c6bf7b41ad0663c
Wayde_Wan <137713243+WaydeWan@users.noreply.github.com>
2026-01-05 23:54:08 -0800

    add hard constraints

--- InfoMatching_Team4/my_app/app.py
+++ InfoMatching_Team4/my_app/app.py
@@ -1,5 +1,14 @@
-import streamlit as st
+import os
+import sys
 
+# Set environment variables early to avoid Windows stdout issues with tqdm
+# This must be done BEFORE importing any model-related modules
+if sys.platform == 'win32':
+    os.environ['HF_HUB_DISABLE_PROGRESS_BARS'] = '1'
+    os.environ['TRANSFORMERS_VERBOSITY'] = 'error'
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+
+import streamlit as st
 
 st.set_page_config(
     page_title="Job Matching Demo",
